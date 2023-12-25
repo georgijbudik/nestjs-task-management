dfd6c69ea0e3242bb4f403e1712b8cced12d8ec3
georgijbudik <georgijbudik@gmail.com>
2023-12-26 00:07:01 +0100

    update exception-filter

--- src/exception-filters/http-exception.filter.ts
+++ src/exception-filters/http-exception.filter.ts
@@ -1,5 +1,9 @@
-import { ArgumentsHost, ExceptionFilter, HttpException } from '@nestjs/common';
-import { Catch } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
 import { Response, Request } from 'express';
 
 @Catch(HttpException)
@@ -9,6 +13,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+
     const errorDetails = exception.getResponse();
 
     response.status(status).json({
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './exception-filters/exception.filter';
+import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
