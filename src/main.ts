a5b497143e53bd1a2c73041fdea280d4b54c4353
georgijbudik <georgijbudik@gmail.com>
2024-01-27 12:37:40 +0100

    init project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+  const PORT = 3000;
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
